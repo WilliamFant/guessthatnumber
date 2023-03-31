425d5fc527058bcb32c95caf2a7ecbfca1bb56f9
Will Fant <williamfant2023@gmail.com>
2023-03-31 08:01:18 -0500

    yes

---
+++ script.js
@@ -0,0 +1,80 @@
+// guess the number game just make it so the user inputs a number and if its the same as the other thing then say you win
+
+
+let num = Math.floor(Math.random() * 10)
+
+let restartGame = true;
+
+let rangeNum;
+
+let rand;
+
+let lives;
+
+let playAgain;
+
+let cat ="   /\_/\  / >  -\>  (    ^  )<   /______3/3"
+
+let enterNumText = "enter what you want your maximum number to be to guess"
+
+while (restartGame){
+    rangeNum = 0;
+    while (!rangeNum || rangeNum < 1){
+        rangeNum = prompt(enterNumText);
+        rangeNum = parseInt(rangeNum);
+    }
+
+    while (!lives || lives < 1){
+        lives = prompt('How many chances do you need to guess the number?');
+        lives = parseInt(lives);
+    }
+
+    rand = Math.floor(Math.random() * rangeNum + 1)
+    while(lives > 0){
+        guess = prompt('guess the number');
+        while (!guess || guess < 1 || guess > rangeNum || guess == "pickle"){
+            if (guess == "pickle"){
+                alert(`the number is ${rand}`)
+            }
+            guess = parseInt(prompt(`Please enter a number from 1 to ${rangeNum}`));
+        }
+        if(guess == rand){
+            alert(`Congrats you win ${rand} was the number`);
+            lives = 0;
+            break;
+        }else if (guess < rand){
+            alert(`The number is higher than ${guess}`)
+            lives--;
+            if(lives > 0){
+                alert(`You have ${lives} lives left`);
+            }else{
+                alert(`You lose the number was ${rand}`);
+                break;
+            }
+        }else if (guess > rand){
+            alert(`The number is lower than ${guess}`)
+            lives--;
+            if(lives > 0){
+                alert(`You have ${lives} lives left`);
+            }else{
+                alert(`You lose the number was ${rand}`);
+                break;
+            }
+        }else{
+            alert("HOW DID YOU GET HERE?");
+        }
+        playAgain = prompt("Would you like to play again (y or n)");
+        while(true){
+            if(playAgain == 'n'){
+                restartGame = false;
+                break;
+            }else if(playAgain == 'y'){
+                restartGame = true;
+                break;
+            }else {
+                playAgain = prompt("Would you like to play again (y or n)");
+            }
+        }
+    }
+}
+alert(`Thanks for playing!`)
